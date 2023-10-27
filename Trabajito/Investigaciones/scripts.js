09928f8d4574a8259547e19bd56d88d5ccd9101d
jonathancm24 <106281339+jonathancm24@users.noreply.github.com>
2023-10-27 10:45:14 -0500

    Update scripts.js

--- Trabajito/Investigaciones/scripts.js
+++ Trabajito/Investigaciones/scripts.js
@@ -26,7 +26,10 @@ function actualizarTabla() {
 }
 
 agregarProyectoBtn.addEventListener('click', () => {
+    var nuevoTexto2="Agregar Proyecto";
     modal.style.display = 'block';
+    var elemento = document.getElementById("miTitulo");
+     elemento.innerHTML = nuevoTexto2;
 });
 
 cerrarModal.addEventListener('click', () => {
@@ -38,6 +41,7 @@ let proyectoEnEdicion = null; // Variable para rastrear el proyecto en edición
 
 
 proyectoTable.addEventListener('click', (e) => {
+    var nuevoTexto1="Editar Proyecto";
     if (e.target.classList.contains('eliminarBtn')) {
         const index = e.target.getAttribute('data-index');
         proyectos.splice(index, 1);
@@ -46,7 +50,8 @@ proyectoTable.addEventListener('click', (e) => {
     } else if (e.target.classList.contains('editarBtn')) {
         const index = e.target.getAttribute('data-index');
         proyectoEnEdicion = proyectos[index]; // Establecer el proyecto en edición.
-
+        var elemento = document.getElementById("miTitulo"); //cambiar el titulo
+        elemento.innerHTML = nuevoTexto1;
         // Llenar el formulario con los valores del proyecto en edición
         document.getElementById('nombreProyecto').value = proyectoEnEdicion.nombreProyecto;
         document.getElementById('autor').value = proyectoEnEdicion.autor;
@@ -68,9 +73,10 @@ proyectoForm.addEventListener('submit', (e) => {
     const carrera = document.getElementById('carrera').value;
     const facultad = document.getElementById('facultad').value;
     const anio = document.getElementById('anio').value;
-
+    
     if (proyectoEnEdicion) {
         // Si hay un proyecto en edición, actualiza sus valores
+        
         proyectoEnEdicion.nombreProyecto = nombreProyecto;
         proyectoEnEdicion.autor = autor;
         proyectoEnEdicion.descripcion = descripcion;
@@ -78,8 +84,11 @@ proyectoForm.addEventListener('submit', (e) => {
         proyectoEnEdicion.facultad = facultad;
         proyectoEnEdicion.anio = anio;
     } else {
+        
+        
         // Si no hay un proyecto en edición, agrega uno nuevo
         const proyecto = {
+            
             nombreProyecto,
             autor,
             descripcion,
@@ -102,3 +111,4 @@ proyectoForm.addEventListener('submit', (e) => {
 
 // Cargar los proyectos almacenados en localStorage al cargar la página
 actualizarTabla();
+
